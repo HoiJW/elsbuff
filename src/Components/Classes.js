b00c7ef0023dc91676f6e9bb67df965f34dbc581
Hoi Wong <jameshf420@gmail.com>
2023-05-01 13:38:27 -0400

    adding base ideas

--- src/Components/Classes.js
+++ src/Components/Classes.js
@@ -1,42 +1,127 @@
+import Images from "./Images";
 const Classes = () =>{
     return(
         <>
         <header>
-        <div className="wrapper">
-        <h1>Classes</h1>
-        </div>
+            <div className="wrapper">
+                <h1>Classes</h1>
+            </div>
         </header>
         <body>
-            <div className="characters">
-                <div class="class-container">
-                <div class="long-rectangle"><h2>Elsword</h2></div>
-                <div class="square">
-                    <p>KE</p>
+            <div className="wrapper">
+                <div className="characters">
+                    {/* elsword */}
+                    <div class="class-container elboy">
+                    <div class="long-rectangle">
+                        <h2>Elsword</h2>
+                    </div>
+                    <div class="square">
+                        <p>KE</p>
+                        <div className="class-pics">
+                            {Images.map(image => {
+                                if (image.alt === "ke") {
+                                return <img key={image.id} src={image.src} alt={image.alt} />;
+                                } else {
+                                return null;
+                                }
+                            })}
+                        </div>
+                    </div>
+                    <div class="square">
+                        <p>RM</p>
+                        <div className="class-pics">
+                            {Images.map(image => {
+                                if (image.alt === "rm") {
+                                return <img key={image.id} src={image.src} alt={image.alt} />;
+                                } else {
+                                return null;
+                                }
+                            })}
+                        </div>
+                        
+                    </div>
+                    <div class="square">
+                        <p>IM</p>
+                        <div className="class-pics">
+                            {Images.map(image => {
+                                if (image.alt === "im") {
+                                return <img key={image.id} src={image.src} alt={image.alt} />;
+                                } else {
+                                return null;
+                                }
+                            })}
+                        </div>
+                    </div>
+                    <div class="square">
+                        <p>GS</p>
+                        <div className="class-pics">
+                            {Images.map(image => {
+                                if (image.alt === "gs") {
+                                return <img key={image.id} src={image.src} alt={image.alt} />;
+                                } else {
+                                return null;
+                                }
+                            })}
+                        </div>
+                    </div>
+                    </div>
+                    {/* aisha */}
+                    <div class="class-container aisha">
+                    <div class="long-rectangle">
+                        <h2>Aisha</h2>
+                    </div>
+                    <div class="square">
+                        <p>Aes</p>
+                        <div className="class-pics">
+                            {Images.map(image => {
+                                if (image.alt === "aes") {
+                                return <img key={image.id} src={image.src} alt={image.alt} />;
+                                } else {
+                                return null;
+                                }
+                            })}
+                        </div>
+                    </div>
+                    <div class="square">
+                        <p>Oz</p>
+                        <div className="class-pics">
+                            {Images.map(image => {
+                                if (image.alt === "oz") {
+                                return <img key={image.id} src={image.src} alt={image.alt} />;
+                                } else {
+                                return null;
+                                }
+                            })}
+                        </div>
+                        
+                    </div>
+                    <div class="square">
+                        <p>mtm</p>
+                        <div className="class-pics">
+                            {Images.map(image => {
+                                if (image.alt === "mtm") {
+                                return <img key={image.id} src={image.src} alt={image.alt} />;
+                                } else {
+                                return null;
+                                }
+                            })}
+                        </div>
+                    </div>
+                    <div class="square">
+                        <p>LA</p>
+                        <div className="class-pics">
+                            {Images.map(image => {
+                                if (image.alt === "la") {
+                                return <img key={image.id} src={image.src} alt={image.alt} />;
+                                } else {
+                                return null;
+                                }
+                            })}
+                        </div>
+                    </div>
+                    </div>
                 </div>
-                <div class="square">
-                    <p>RM</p>
-                </div>
-                <div class="square">
-                    <p>IM</p>
-                </div>
-                <div class="square">
-                    <p>GS</p>
-                </div>
-                </div>
-                <h2>Aisha</h2>
-                <h2>Rena</h2>
-                <h2>Raven</h2>
-                <h2>Eve</h2>
-                <h2>Chung</h2>
-                <h2>Ara</h2>
-                <h2>Elesis</h2>
-                <h2>Lu/Ciel</h2>
-                <h2>ADD</h2>
-                <h2>Rose</h2>
-                <h2>Ain</h2>
-                <h2>Laby</h2>
-                <h2>Noah</h2>
-            </div>
+            </div>    
         </body>
         </>
     )
---
+++ src/Components/Images.js
@@ -0,0 +1,54 @@
+import ke from '../partials/asset/KE.png';
+import rm from '../partials/asset/RM.png';
+import im from '../partials/asset/IM.png';
+import gs from '../partials/asset/GS.png';
+import aes from '../partials/asset/Aes.png';
+import oz from '../partials/asset/Oz.png';
+import mtm from '../partials/asset/Mtm.png';
+import la from '../partials/asset/LA.png';
+
+const images = [
+  {
+    id: 1,
+    src: ke,
+    alt: 'ke'
+  },
+  {
+    id: 2,
+    src: rm,
+    alt: 'rm'
+  },
+  {
+    id: 3,
+    src: im,
+    alt: 'im'
+  },
+  {
+    id: 4,
+    src: gs,
+    alt: 'gs'
+  },
+  {
+    id: 5,
+    src: aes,
+    alt: 'aes'
+  },
+  {
+    id: 6,
+    src: oz,
+    alt: 'oz'
+  },
+  {
+    id: 7,
+    src: mtm,
+    alt: 'mtm'
+  },
+  {
+    id: 8,
+    src: la,
+    alt: 'la'
+  },
+
+];
+
+export default images;
